8aa0e18fd4c9cd51498ea702efb989a33ffedf82
Tefan Haetami <tefanhaetami87@gmail.com>
2025-01-30 08:03:58 +0700

    Add image compression functionality to image upload service

--- src/services/image_service.rs
+++ src/services/image_service.rs
@@ -1,5 +1,7 @@
 use actix_multipart::form::{tempfile::TempFile, MultipartForm};
+use actix_web::error::ErrorInternalServerError;
 use actix_web::{post, Error, HttpResponse, Responder};
+use image::ImageReader;
 
 #[derive(Debug, MultipartForm)]
 pub struct UploadForm {
@@ -13,7 +15,17 @@ pub async fn save_files(
 ) -> Result<impl Responder, Error> {
     for f in form.files {
         let path = format!("./tmp/{}", f.file_name.unwrap());
-        f.file.persist(path).unwrap();
+        f.file.persist(&path).unwrap();
+        let img = ImageReader::open(path)
+            .map_err(ErrorInternalServerError)?
+            .decode()
+            .map_err(ErrorInternalServerError)?;
+        let rgb_image = img.to_rgb8();
+
+        let output_path = format!("./tmp/{}", "compressed.jpg");
+        rgb_image
+            .save(output_path)
+            .map_err(ErrorInternalServerError)?;
     }
     Ok(HttpResponse::Ok())
 }
